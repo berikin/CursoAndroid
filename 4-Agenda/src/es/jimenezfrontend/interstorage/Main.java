e85621c4c86ff8a7db18422f9e09f3763c4a4a36
José Antonio Yáñez Jiménez <jose@jimenezfrontend.es>
2014-01-28 13:34:04 +0100

    halt exercise four

---
+++ 4-Agenda/src/es/jimenezfrontend/interstorage/Main.java
@@ -0,0 +1,84 @@
+package es.jimenezfrontend.interstorage;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Main extends Activity {
+
+	public Button btn;
+	public EditText txtNombre, txtTelefono;
+
+	@Override
+	protected void onCreate ( Bundle savedInstanceState ) {
+		super.onCreate ( savedInstanceState );
+		setContentView ( R.layout.layout_main );
+
+		btn = ( Button ) findViewById ( R.id.button1 );
+		txtNombre = ( EditText ) findViewById ( R.id.editText1 );
+		txtTelefono = ( EditText ) findViewById ( R.id.editText2 );
+
+		btn.setOnClickListener ( new OnClickListener () {
+			@Override
+			public void onClick ( View v ) {
+				escribirFichero ();
+			}
+		} );
+
+	}// FIN ONCREATE
+
+	protected void escribirFichero () {
+		OutputStreamWriter escritor = null;
+		try {
+			File f = new File ( Environment.getDataDirectory () + "/data/personas/personas.txt" );
+			Log.d ( "dir", Environment.getDataDirectory () + "/data/personas/personas.txt" );
+			Log.d ( "existe", f.exists () + "" );
+			if ( f.exists () ) {
+				FileInputStream fin = new FileInputStream ( f );
+				int c;
+				String temp = "";
+				while ( ( c = fin.read () ) != -1 ) {
+					temp = temp + Character.toString ( ( char ) c );
+				}
+				fin.close ();
+				Log.d ( "tem", temp );
+				escritor = new OutputStreamWriter ( new FileOutputStream ( f ) );
+				escritor.write ( temp );
+				escritor.write ( txtNombre.getText ().toString () + "-" );
+				escritor.write ( txtTelefono.getText ().toString () + "\n" );
+			}
+			else {
+				escritor = new OutputStreamWriter ( new FileOutputStream ( f ) );
+				escritor.write ( txtNombre.getText ().toString () + "-" );
+				escritor.write ( txtTelefono.getText ().toString () + "\n" );
+			}
+			txtNombre.setText ( "" );
+			txtTelefono.setText ( "" );
+		}
+		catch ( Exception e ) {
+			Log.e ( "Pepita", e.toString () );
+		}
+		finally {
+			try {
+				if ( escritor != null ) {
+					escritor.close ();
+				}
+			}
+			catch ( IOException ex ) {
+				ex.printStackTrace ();
+			}
+		}
+	}// FIN ESCRIBIRFICHERO()
+}
